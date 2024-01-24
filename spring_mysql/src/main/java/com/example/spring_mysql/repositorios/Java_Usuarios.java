a04af4b6e70e50f77f01018ffff2cda090268a39
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-23 23:42:41 -0500

    Update Java_Usuarios.java

--- spring_mysql/src/main/java/com/example/spring_mysql/repositorios/Java_Usuarios.java
+++ spring_mysql/src/main/java/com/example/spring_mysql/repositorios/Java_Usuarios.java
@@ -1,4 +1,10 @@
 package com.example.spring_mysql.repositorios;
 
-public interface Java_Usuarios {
+import com.example.spring_mysql.modelos.JavaUsuarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Java_Usuarios extends JpaRepository<JavaUsuarios, Long> {
+
 }
