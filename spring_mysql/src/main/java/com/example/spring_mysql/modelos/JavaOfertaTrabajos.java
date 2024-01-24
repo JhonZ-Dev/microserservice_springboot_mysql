930289c2369a5e245b3e5f89e63e210580d95a84
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-23 23:24:24 -0500

    Update JavaOfertaTrabajos.java

--- spring_mysql/src/main/java/com/example/spring_mysql/modelos/JavaOfertaTrabajos.java
+++ spring_mysql/src/main/java/com/example/spring_mysql/modelos/JavaOfertaTrabajos.java
@@ -1,4 +1,24 @@
 package com.example.spring_mysql.modelos;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "java_ofertas_trabajo")
 public class JavaOfertaTrabajos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_ofertas_trabajo;
+    private String java_oportunidad;
+    private String java_descripcion;
+
+    private String java_tags;
+
+    //relacion de muchos a uno con company y con id_usuario de compañia (la foranea)
+    @ManyToOne
+    @JoinColumn(name = "id_usuarios")
+    private JavaCompania id_usuarios;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compania")
+    private JavaCompania id_compania;
 }
