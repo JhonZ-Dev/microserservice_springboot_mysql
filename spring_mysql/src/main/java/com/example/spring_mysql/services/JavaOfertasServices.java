c9a807b782c26a111f4de11145ff0c705438e5fe
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-24 00:08:25 -0500

    Update JavaOfertasServices.java

--- spring_mysql/src/main/java/com/example/spring_mysql/services/JavaOfertasServices.java
+++ spring_mysql/src/main/java/com/example/spring_mysql/services/JavaOfertasServices.java
@@ -19,13 +19,36 @@ public class JavaOfertasServices {
 
     //metodo para guardar con usuario y compañia
     //obtener el id_compañia
-    public JavaOfertaTrabajos guardar(JavaOfertaTrabajos trabajos,
-                                      Long id_compania,
-                                      Long id_usuarios){
+    public JavaOfertaTrabajos guardar(JavaOfertaTrabajos trabajos, Long id_compania, Long id_usuarios) {
+        // Obtener instancias de JavaCompania para id_compania e id_usuarios
+        Optional<JavaCompania> optionalCompania = javaCompanyRepo.findById(id_compania);
+        Optional<JavaCompania> optionalUsuario = javaCompanyRepo.findById(id_usuarios);
 
-        //obtener el id_compnia
-        Optional<JavaCompania> optional = javaCompanyRepo.findById(id_compania);
-        //obtener 
+        // Verificar si ambas instancias existen
+        if (optionalCompania.isPresent() && optionalUsuario.isPresent()) {
+            JavaCompania javaCompania = optionalCompania.get();
+            JavaCompania javaUsuario = optionalUsuario.get();
 
+            // Asignar las instancias de JavaCompania a la oferta de trabajo
+            trabajos.setJavaCompania(javaCompania);
+
+            // Si necesitas asignar directamente los valores de id_usuarios e id_compania a la oferta de trabajo, hazlo aquí
+            // trabajos.setId_usuarios(javaUsuario);
+            // trabajos.setId_compania(javaCompania);
+
+            // Guardar la oferta de trabajo
+            return repositorio.save(trabajos);
+        } else {
+            // Manejar el caso en el que alguna de las instancias no existe
+            throw new RuntimeException("No se encontró una o ambas instancias de JavaCompania.");
+        }
+    }
+    public JavaOfertaTrabajos guardarOfertaTrabajo(JavaOfertaTrabajos ofertaTrabajo) {
+        // Asignar el objeto JavaCompania y valores de las columnas de clave externa
+        JavaCompania javaCompania = ofertaTrabajo.getJavaCompania();
+        ofertaTrabajo.setJavaCompania(javaCompania);
+
+        // Guardar la oferta de trabajo
+        return repositorio.save(ofertaTrabajo);
     }
 }
