8dd750150b33ba7550fa2f1c474a496795408f4a
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-23 23:10:55 -0500

    Create

--- spring_mysql/src/main/java/com/example/spring_mysql/modelos/JavaUsuarios.java
+++ spring_mysql/src/main/java/com/example/spring_mysql/modelos/JavaUsuarios.java
@@ -1,4 +1,18 @@
 package com.example.spring_mysql.modelos;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_java_usuarios")
+@Data
 public class JavaUsuarios {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_usuarios;
+    private String java_email;
+    private Boolean java_activacion;
+    private String java_contrasenia;
+
+
 }
