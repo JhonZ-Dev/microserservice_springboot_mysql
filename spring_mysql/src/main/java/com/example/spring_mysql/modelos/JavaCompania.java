c5a143b655d64800fb2fa44d18e52eae70e7cce1
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-23 23:15:03 -0500

    Update JavaCompania.java

--- spring_mysql/src/main/java/com/example/spring_mysql/modelos/JavaCompania.java
+++ spring_mysql/src/main/java/com/example/spring_mysql/modelos/JavaCompania.java
@@ -1,5 +1,23 @@
 package com.example.spring_mysql.modelos;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Table(name = "tbl_java_compañia")
+@Data
 public class JavaCompania {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_compania;
+    private String java_actividades;
+    private int java_fundacion;
+    private String java_descripcion;
+
+    //relacion de uno a uno con usuarios
+    //debia aparecer aqui el id_usaurio;
+    @OneToOne
+    @JoinColumn(name = "id_usuarios")
+    private JavaUsuarios id_usuarios;
+
 }
